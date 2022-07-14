8f59023733547a489a2a5dbb6c529d0a2f50620b
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-07-14 11:50:20 +0530

    Update cryptoApi.js

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -6,7 +6,7 @@ const REACT_APP_APIKey = process.env.REACT_APP_APIKey;
 const cryptoApiHeaders = {
     'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
     // 'X-RapidAPI-Key': 'b1d57f69b3mshadfcecfc9e489a6p14b18cjsn87ec366e56b2'
-    REACT_APP_APIKey: 'b1d57f69b3mshadfcecfc9e489a6p14b18cjsn87ec366e56b2'
+    'X-RapidAPI-Key': REACT_APP_APIKey
 }
 
 const baseUrl = 'https://coinranking1.p.rapidapi.com/';
